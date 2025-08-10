8558c006966b90b4336177d10d1775bd4e831c56
jenny <jenny.finsrud@gmail.com>
2025-08-10 11:34:29 +0200

    ...

--- tools_j/create_thumb.py
+++ tools_j/create_thumb.py
@@ -1,13 +1,12 @@
 import pyvips
 from pathlib import Path
 
-def create_thumbnail(input_tiff_path : str | Path, output_thumbnail_path : str | Path, thumbnail_width : int):
+def create_thumbnail(input_tiff_path : str | Path, output_thumbnail_path : str | Path):
     # Load the TIFF image
     image = pyvips.Image.new_from_file(input_tiff_path, access='sequential')
 
     # Calculate the scale needed to resize the image
-    scale = thumbnail_width / image.width
-    
+    scale = 0.2
     # Resize the image to the thumbnail size
     thumbnail = image.resize(scale)
 
@@ -16,8 +15,10 @@ def create_thumbnail(input_tiff_path : str | Path, output_thumbnail_path : str |
 
 if __name__ == "__main__":
     # Input and output path
-    input_tiff_path = "/media/.../Pyramidal_TIFF_files/.../Pyramidal_image.tif"
-    output_thumbnail_path = "/media/.../thumbnail/Pyramidal_image.png"
-    thumbnail_width = 2048  # Width of thumbnail
+    input_tiff_path = "/media/jenny/Expansion/MM_HE_results/HE_MM179_2D_290125_20x_BF_01/mask/wsi/whole_image_complete.png"
+    # input_tiff_path = "/media/jenny/Expansion/aSMA/Image_MM179_A_SMA.tif"
+    # input_tiff_path = "/media/jenny/Expansion/MM_HE_20x_TIFF/HE_MM179_A_70225_vsi_Collection/HE_MM179_A_70225_20x_BF_01/HE_MM179_A_70225_20x_BF_01.tif"
+    output_thumbnail_path = "/media/jenny/Expansion/MM_HE_thumbnail/MM179_2D/result_HE_MM179_2D_290125_20x_BF_01.png"
+    
 
-    create_thumbnail(input_tiff_path, output_thumbnail_path, thumbnail_width)
+    create_thumbnail(input_tiff_path, output_thumbnail_path)
